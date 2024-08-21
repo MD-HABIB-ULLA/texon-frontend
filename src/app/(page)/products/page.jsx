d38225e4b5729c71fef50304ddb0d7968fd0f2ae
peuldev <peul.islam71@gmail.com>
2024-08-21 23:45:55 +0600

    perfect plan aded

--- src/app/(page)/products/page.jsx
+++ src/app/(page)/products/page.jsx
@@ -1,5 +1,6 @@
 import AmazingFeatures from "@/components/pages/Products/amazingFeatures/amazingFeatures";
 import OurSolution from "@/components/pages/Products/ourSolution/ourSolution";
+import PerfectPlan from "@/components/pages/Products/perfectPlan/perfectPlan";
 import Support from "@/components/pages/Products/Support/Support";
 import TrustedCompanies from "@/components/pages/Products/trustedCompanies/trustedCompanies";
 
@@ -10,6 +11,7 @@ export default function Products() {
       <Support />
       <AmazingFeatures />
       <TrustedCompanies />
+      <PerfectPlan />
     </div>
   );
 }
---
+++ src/components/pages/Products/perfectPlan/perfectPlan.jsx
@@ -0,0 +1,31 @@
+import HeadingTitle from "@/components/common/title/headingTitle";
+
+export default function PerfectPlan() {
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <div className="px-2">
+        <div>
+          <HeadingTitle
+            heading={<>Pick the perfect plan</>}
+            subHeading={
+              <>
+                <p>
+                  {" "}
+                  You can compare the pricing models and choose the best one for
+                  your store on our <br /> website. You can also upgrade or downgrade
+                  anytime according to your needs.
+                </p>
+                <p className="pt-5">
+                  So what are you waiting for? Don’t let this opportunity pass
+                  you by. Contact us <br /> today and get ready to launch your online
+                  store with our readymade ecommerce <br /> website and app solution.
+                  You won’t regret it
+                </p>
+              </>
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
