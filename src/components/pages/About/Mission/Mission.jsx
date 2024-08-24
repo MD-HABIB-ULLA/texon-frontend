140b74dd342420bb50f83e89a5c046b0c8150b51
peuldev <peul.islam71@gmail.com>
2024-08-24 13:24:15 +0600

    all update

--- src/components/pages/About/Mission/Mission.jsx
+++ src/components/pages/About/Mission/Mission.jsx
@@ -5,45 +5,47 @@ import commonsense from "../../../../../public/about/zohoaboutus-commonsense 1.s
 
 export default function Mission() {
   return (
-    <div className="max-w-screen-xl mx-auto px-4 py-10 md:pb-10 lg:pb-20">
-      <div className="bg-primry text-white p-2 md:p-5 lg:p-10 rounded-lg shadow-lg space-y-16">
-        <div className="flex flex-col md:flex-row md:space-x-8">
-          <div className="md:w-1/2 space-y-4">
-            <div className="flex items-center space-x-3">
-              <h1 className="text-2xl font-bold">MISSION</h1>
-              <Image src={icon} alt="Icon" className="w-6" />
+    <div className="md:mt-10">
+      <div className="max-w-screen-xl mx-auto px-4 py-10 md:pb-10 lg:pb-20">
+        <div className="bg-primry text-white p-2 md:p-5 lg:p-10 rounded-lg shadow-lg space-y-16">
+          <div className="flex flex-col md:flex-row md:space-x-8">
+            <div className="md:w-1/2 space-y-4">
+              <div className="flex items-center space-x-3">
+                <h1 className="text-2xl font-bold">MISSION</h1>
+                <Image src={icon} alt="Icon" className="w-6" />
+              </div>
+              <p>
+                At Texon Limited, our mission is to deliver bespoke software
+                solutions that surpass client expectations and propel their
+                success in the dynamic digital landscape. We are dedicated to
+                leveraging the latest technologies, fostering a culture of
+                continuous learning and innovation, and building enduring
+                partnerships.
+              </p>
             </div>
-            <p>
-              At Texon Limited, our mission is to deliver bespoke software
-              solutions that surpass client expectations and propel their
-              success in the dynamic digital landscape. We are dedicated to
-              leveraging the latest technologies, fostering a culture of
-              continuous learning and innovation, and building enduring
-              partnerships.
-            </p>
+            <div className="md:w-1/2 mt-6 md:mt-0">
+              <Image src={commonsense} alt="commonsense" />
+            </div>
           </div>
-          <div className="md:w-1/2 mt-6 md:mt-0">
-            <Image src={commonsense} alt="commonsense" />
-          </div>
-        </div>
 
-        <div className="flex flex-col-reverse md:flex-row md:space-x-8">
-          <div className="md:w-1/2 mb-6 md:mb-0">
-            <Image src={commitedour} alt="commitedour" />
-          </div>
-          <div className="md:w-1/2 space-y-4">
-            <div className="flex items-center space-x-3">
-              <h1 className="text-2xl font-bold">MISSION</h1>
-              <Image src={icon} alt="Icon" className="w-6" />
+          <div className="flex flex-col-reverse md:flex-row md:space-x-8">
+            <div className="md:w-1/2 mb-6 md:mb-0">
+              <Image src={commitedour} alt="commitedour" />
             </div>
-            <p className="pb-5 md:mb-0 lg:pb-0">
-              At Texon Limited, our mission is to deliver bespoke software
-              solutions that surpass client expectations and propel their
-              success in the dynamic digital landscape. We are dedicated to
-              leveraging the latest technologies, fostering a culture of
-              continuous learning and innovation, and building enduring
-              partnerships.
-            </p>
+            <div className="md:w-1/2 space-y-4">
+              <div className="flex items-center space-x-3">
+                <h1 className="text-2xl font-bold">MISSION</h1>
+                <Image src={icon} alt="Icon" className="w-6" />
+              </div>
+              <p className="pb-5 md:mb-0 lg:pb-0">
+                At Texon Limited, our mission is to deliver bespoke software
+                solutions that surpass client expectations and propel their
+                success in the dynamic digital landscape. We are dedicated to
+                leveraging the latest technologies, fostering a culture of
+                continuous learning and innovation, and building enduring
+                partnerships.
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/About/texonVideo/texonVideo.jsx
+++ src/components/pages/About/texonVideo/texonVideo.jsx
@@ -12,36 +12,39 @@ export default function TexonVideo() {
   };
 
   return (
-    <div className="max-w-screen-xl mx-auto bg-primry rounded shadow-lg">
-      <div className="px-2 py-2">
-        <div className="flex justify-center">
-          {isPlaying ? (
-            <iframe
-              width="100%"
-              height="500"
-              src="https://www.youtube.com/embed/m050iy5_2ng?si=Ddwss6WfSmIcZA1i&autoplay=1"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-              className="max-w-screen-xl"
-            ></iframe>
-          ) : (
-            <div
-              className="relative cursor-pointer max-w-screen-xl h-[500px] flex items-center justify-center"
-              onClick={handleThumbnailClick}
-            >
-              <Image
-                src={texon_logo}
-                alt="Texon Logo"
-                className="w-full h-auto object-contain"
-              />
-              <div className="absolute inset-0 flex items-center justify-center">
-                <FaPlayCircle className="w-24 h-24 text-white" />
+    <div className="my-10 lg:my-20">
+      {" "}
+      <div className="max-w-screen-xl mx-auto bg-primry rounded shadow-lg">
+        <div className="px-2 py-2">
+          <div className="flex justify-center">
+            {isPlaying ? (
+              <iframe
+                width="100%"
+                height="500"
+                src="https://www.youtube.com/embed/m050iy5_2ng?si=Ddwss6WfSmIcZA1i&autoplay=1"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+                className="max-w-screen-xl"
+              ></iframe>
+            ) : (
+              <div
+                className="relative cursor-pointer max-w-screen-xl h-[500px] flex items-center justify-center"
+                onClick={handleThumbnailClick}
+              >
+                <Image
+                  src={texon_logo}
+                  alt="Texon Logo"
+                  className="w-full h-auto object-contain"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <FaPlayCircle className="w-24 h-24 text-white" />
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
