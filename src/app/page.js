fd2be7f1a1c19c6b29c1508afe5a3e82fb7935a6
peuldev <peul.islam71@gmail.com>
2024-08-24 16:58:35 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
     <main>
       <OurCompany />
       <NextLevel />
-      <CoreServices />
       <OurPreviousWork />
+      <CoreServices />
       <SoftwareProducts />
       <DevelopmentServices />
       <AboutTexon />
