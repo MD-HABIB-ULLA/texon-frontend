260325ae23b3bf45563d435d0fe04b93762bdb2a
peuldev <peul.islam71@gmail.com>
2024-08-24 14:55:28 +0600

    content update executive team and responsive

--- src/components/pages/About/executiveTeam/executiveTeam.jsx
+++ src/components/pages/About/executiveTeam/executiveTeam.jsx
@@ -1,9 +1,9 @@
 import HeadingTitle from "@/components/common/title/headingTitle";
 import Image from "next/image";
-import Team1 from "../../../../../public/about/Mask group.svg";
 import Team2 from "../../../../../public/about/Mask group (1).svg";
 import Team3 from "../../../../../public/about/Mask group (2).svg";
 import Team4 from "../../../../../public/about/Mask group (3).svg";
+import Team1 from "../../../../../public/about/Mask group.svg";
 
 export default function ExecutiveTeam() {
   return (
@@ -31,12 +31,12 @@ export default function ExecutiveTeam() {
         <div className="lg:flex lg:place-items-end">
           <Image
             src={Team2}
-            alt="Team1"
+            alt="Team2"
             className="object-cover mb-5 md:mb-0 lg:mb-0 pr-0 md:pr-5 lg:pr-5"
           />
           <div>
             <h1 className="lg:text-2xl md:text-2xl text-xl font-semibold">
-              Fabien Pinckaers • CEO
+              Antony Lesuisse • CTO
             </h1>
             <p className="mt-2">
               Antony is one of the lucky people in life who can say he loves
@@ -45,15 +45,15 @@ export default function ExecutiveTeam() {
             </p>
           </div>
         </div>
-        <div className="lg:flex lg:place-items-end">
+        <div className="lg:flex lg:place-items-end mt-0 md:mt-10 lg:mt-10">
           <Image
             src={Team4}
-            alt="Team1"
+            alt="Team4"
             className="object-cover mb-5 md:mb-0 lg:mb-0 pr-0 md:pr-5 lg:pr-5"
           />
           <div>
             <h1 className="lg:text-2xl md:text-2xl text-xl font-semibold">
-              Fabien Pinckaers • CEO
+              Alessandro Mazzocchetti • CFO
             </h1>
             <p className="mt-2">
               Antony is one of the lucky people in life who can say he loves
@@ -65,12 +65,12 @@ export default function ExecutiveTeam() {
         <div className="lg:flex lg:place-items-end">
           <Image
             src={Team3}
-            alt="Team1"
+            alt="Team5"
             className="object-cover mb-5 md:mb-0 lg:mb-0 pr-0 md:pr-5 lg:pr-5"
           />
           <div>
             <h1 className="lg:text-2xl md:text-2xl text-xl font-semibold">
-              Fabien Pinckaers • CEO
+              Sébastien Bruyr • CCO
             </h1>
             <p className="mt-2">
               Antony is one of the lucky people in life who can say he loves
