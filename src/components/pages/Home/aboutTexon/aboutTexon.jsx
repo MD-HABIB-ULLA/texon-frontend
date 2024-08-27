e790924a617fcda6f29a7dda7e13c73d70e3b171
peuldev <peul.islam71@gmail.com>
2024-08-28 00:42:29 +0600

    update

--- src/components/pages/Home/aboutTexon/aboutTexon.jsx
+++ src/components/pages/Home/aboutTexon/aboutTexon.jsx
@@ -9,8 +9,8 @@ export default function AboutTexon() {
     <div className="max-w-screen-xl mx-auto py-5 md:py-10 lg:py-20">
       <div className="px-4">
         <div className="flex justify-between lg:space-x-8">
-          <div className="space-y-7 lg:w-1/2">
-            <h1 className="font-medium text-xl w-fit">About Texon</h1>
+          <div className="space-y-7 lg:w-1/2 text-center lg:text-start">
+            <h1 className="font-medium text-xl w-auto text-center lg:text-start">About Texon</h1>
             <h3 className="text-4xl md:text-5xl font-semibold">Who We Are?</h3>
             <p>
               Texon limited is a custom software development company that we are
@@ -33,7 +33,7 @@ export default function AboutTexon() {
                 alt="it_image_1"
                 width={200}
                 height={200}
-                className="object-contain"
+                className="object-contain hidden lg:block"
               />
             </div>
             <div className="flex justify-center items-center mb-20 -mt-10">
@@ -42,7 +42,7 @@ export default function AboutTexon() {
                 alt="it_image_2"
                 width={200}
                 height={200}
-                className="object-contain"
+                className="object-contain hidden lg:block"
               />
             </div>
             <div className="flex justify-center items-center mb-20">
@@ -51,7 +51,7 @@ export default function AboutTexon() {
                 alt="it_image_3"
                 width={200}
                 height={200}
-                className="object-contain"
+                className="object-contain hidden lg:block"
               />
             </div>
             <div className="flex justify-center items-center -mt-48">
@@ -60,7 +60,7 @@ export default function AboutTexon() {
                 alt="it_image_4"
                 width={200}
                 height={200}
-                className="object-contain"
+                className="object-contain hidden lg:block"
               />
             </div>
           </div>
