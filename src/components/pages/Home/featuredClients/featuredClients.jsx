e9f50ddd95c68355a6c8002704bc137761f28893
peuldev <peul.islam71@gmail.com>
2024-08-27 22:45:39 +0600

    update

--- src/components/pages/Home/featuredClients/featuredClients.jsx
+++ src/components/pages/Home/featuredClients/featuredClients.jsx
@@ -99,6 +99,87 @@ export default function FeaturedClients() {
             </div>
           </Marquee>
         </div>
+        <div className="mt-10">
+          <Marquee pauseOnHover speed={60} gradient={false} direction="right">
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={shukhimart}
+                  alt="shukhimart"
+                  layout="intrinsic"
+                  width={100}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={optimal}
+                  alt="optimal"
+                  layout="intrinsic"
+                  width={100}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={alpha}
+                  alt="relax_bazar"
+                  layout="intrinsic"
+                  width={70}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={mawts}
+                  alt="relax_bazar"
+                  layout="intrinsic"
+                  width={70}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={relax_bazar}
+                  alt="relax_bazar"
+                  layout="intrinsic"
+                  width={100}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={hb_aviation}
+                  alt="relax_bazar"
+                  layout="intrinsic"
+                  width={100}
+                  height={60}
+                />
+              </div>
+            </div>
+            <div className="flex items-center px-3">
+              <div className="bg-white px-5 py-10 w-40 h-32 flex items-center justify-center">
+                <Image
+                  src={alpha}
+                  alt="relax_bazar"
+                  layout="intrinsic"
+                  width={70}
+                  height={60}
+                />
+              </div>
+            </div>
+          </Marquee>
+        </div>
       </div>
     </div>
   );
