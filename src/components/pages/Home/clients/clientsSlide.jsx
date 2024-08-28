6cdac9f7c2f104fd4185aa3607a310e362c71f54
peuldev <peul.islam71@gmail.com>
2024-08-28 19:38:32 +0600

    client slide aded

--- src/components/pages/Home/clients/clientsSlide.jsx
+++ src/components/pages/Home/clients/clientsSlide.jsx
@@ -9,7 +9,7 @@ import "swiper/css/pagination";
 import client from "../../../../../public/images/client.svg";
 import "./styles.css";
 
-// import required modules
+// Import required modules
 import Image from "next/image";
 import { FreeMode, Pagination } from "swiper/modules";
 
@@ -17,7 +17,7 @@ export default function ClientsSlide() {
   return (
     <>
       <Swiper
-        slidesPerView={2}
+        slidesPerView={1.33} // Show slides with approximately 75% width
         spaceBetween={30}
         freeMode={true}
         pagination={{
@@ -27,15 +27,15 @@ export default function ClientsSlide() {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image src={client} alt="client" width={100} height={100} />
-            <div className="ml-4 text-left space-y-1">
+          <div className="flex justify-center">
+            <Image src={client} alt="client" width={100} height={5} />
+            <div className="ml-4 text-left space-y-3 py-5">
               <h4 className="text-sm text-[#525252]">May 8, 2020</h4>
-              <h1 className="text-2xl font-bold">Mehedi Shamim</h1>
+              <h1 className="text-3xl font-bold">Mehedi Shamim</h1>
               <h3 className="text-base font-medium">
-                Head of Brand, Marketing, and Strategy at Apex Property
+                Head of Brand, Marketing, and Strategy <br /> at Apex Property
               </h3>
-              <p className="text-lg">
+              <p className="">
                 The Texon Limited team did an excellent job delivering a custom
                 software solution that fit our specific needs. Their technical
                 expertise and attention to detail made the project a success. We
@@ -46,15 +46,15 @@ export default function ClientsSlide() {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image src={client} alt="client" width={100} height={100} />
-            <div className="ml-4 text-left space-y-1">
+          <div className="flex justify-center">
+            <Image src={client} alt="client" width={100} height={5} />
+            <div className="ml-4 text-left space-y-3 py-5">
               <h4 className="text-sm text-[#525252]">May 8, 2020</h4>
-              <h1 className="text-2xl font-bold">Mehedi Shamim</h1>
+              <h1 className="text-3xl font-bold">Mehedi Shamim</h1>
               <h3 className="text-base font-medium">
-                Head of Brand, Marketing, and Strategy at Apex Property
+                Head of Brand, Marketing, and Strategy <br /> at Apex Property
               </h3>
-              <p className="text-lg">
+              <p className="">
                 The Texon Limited team did an excellent job delivering a custom
                 software solution that fit our specific needs. Their technical
                 expertise and attention to detail made the project a success. We
@@ -65,15 +65,15 @@ export default function ClientsSlide() {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image src={client} alt="client" width={100} height={100} />
-            <div className="ml-4 text-left space-y-1">
+          <div className="flex justify-center">
+            <Image src={client} alt="client" width={100} height={5} />
+            <div className="ml-4 text-left space-y-3 py-5">
               <h4 className="text-sm text-[#525252]">May 8, 2020</h4>
-              <h1 className="text-2xl font-bold">Mehedi Shamim</h1>
+              <h1 className="text-3xl font-bold">Mehedi Shamim</h1>
               <h3 className="text-base font-medium">
-                Head of Brand, Marketing, and Strategy at Apex Property
+                Head of Brand, Marketing, and Strategy <br /> at Apex Property
               </h3>
-              <p className="text-lg">
+              <p className="">
                 The Texon Limited team did an excellent job delivering a custom
                 software solution that fit our specific needs. Their technical
                 expertise and attention to detail made the project a success. We
@@ -83,44 +83,7 @@ export default function ClientsSlide() {
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image src={client} alt="client" width={100} height={100} />
-            <div className="ml-4 text-left space-y-1">
-              <h4 className="text-sm text-[#525252]">May 8, 2020</h4>
-              <h1 className="text-2xl font-bold">Mehedi Shamim</h1>
-              <h3 className="text-base font-medium">
-                Head of Brand, Marketing, and Strategy at Apex Property
-              </h3>
-              <p className="text-lg">
-                The Texon Limited team did an excellent job delivering a custom
-                software solution that fit our specific needs. Their technical
-                expertise and attention to detail made the project a success. We
-                were impressed with their ability to deliver the project on time
-                and within budget.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image src={client} alt="client" width={100} height={100} />
-            <div className="ml-4 text-left space-y-1">
-              <h4 className="text-sm text-[#525252]">May 8, 2020</h4>
-              <h1 className="text-2xl font-bold">Mehedi Shamim</h1>
-              <h3 className="text-base font-medium">
-                Head of Brand, Marketing, and Strategy at Apex Property
-              </h3>
-              <p className="text-lg">
-                The Texon Limited team did an excellent job delivering a custom
-                software solution that fit our specific needs. Their technical
-                expertise and attention to detail made the project a success. We
-                were impressed with their ability to deliver the project on time
-                and within budget.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
+       
       </Swiper>
     </>
   );
