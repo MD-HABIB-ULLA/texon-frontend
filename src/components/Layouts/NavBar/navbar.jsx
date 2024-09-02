4c692f6e72188cdff883a7890bb6006a33698a33
peuldev <peul.islam71@gmail.com>
2024-09-02 13:36:15 +0600

    aded sm divices navbar

--- src/components/Layouts/NavBar/navbar.jsx
+++ src/components/Layouts/NavBar/navbar.jsx
@@ -276,15 +276,27 @@ const Navbar = () => {
     }
   };
 
+  //   sm
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isItem1SubmenuOpen, setIsItem1SubmenuOpen] = useState(false);
+
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+  const toggleItem1Submenu = () => setIsItem1SubmenuOpen(!isItem1SubmenuOpen);
   return (
     <div>
       <div className="navbar max-w-screen-xl mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="lg:hidden">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden"
+              onClick={toggleDropdown}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 md:h-8 md:w-8"
+                className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -297,32 +309,100 @@ const Navbar = () => {
                 />
               </svg>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+            {isDropdownOpen && (
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-white z-[1] mt-3 w-screen p-2 shadow"
+              >
+                <li
+                  onMouseEnter={() => setIsItem1SubmenuOpen(true)}
+                  onMouseLeave={() => setIsItem1SubmenuOpen(false)}
+                >
+                  <div className="flex justify-between items-center">
+                    <a>Services</a>
+                    <button onClick={toggleItem1Submenu}>
+                      {isItem1SubmenuOpen ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      )}
+                    </button>
+                  </div>
+                  {isItem1SubmenuOpen && (
+                    <div className="p-2 block">
+                      <div className="flex items-center pb-2">
+                        <Image src={development} width={20} alt="development" />
+                        <p className="ms-2">Software Development</p>
+                      </div>
+                      <div className="flex items-center pb-2">
+                        <Image src={web} width={20} alt="web" />
+                        <p className="ms-2">Web Development</p>
+                      </div>
+                      <div className="flex items-center pb-2">
+                        <Image src={app} width={20} alt="app" />
+                        <p className="ms-2">Mobile App Develop</p>
+                      </div>
+                      <div className="flex items-center pb-2">
+                        <Image src={management} width={20} alt="management" />
+                        <p className="ms-2">Management Software</p>
+                      </div>
+                      <div className="flex items-center pb-2">
+                        <Image src={ai} width={20} alt="ai" />
+                        <p className="ms-2">Ai & Automation</p>
+                      </div>
+                      <div className="flex items-center pb-2">
+                        <Image src={aws} width={20} alt="aws" />
+                        <p className="ms-2">AWS Services</p>
+                      </div>
+                      <div className="flex items-center pb-2">
+                        <Image src={development} width={20} alt="development" />
+                        <p className="ms-2">IT Infrastructure</p>
+                      </div>
+                    </div>
+                  )}
+                </li>
+                <li>
+                  <a>Products</a>
+                </li>
+                <li>
+                  <a>Case Studies</a>
+                </li>
+                <li>
+                  <a>About</a>
+                </li>
+                <li>
+                  <a>Contact</a>
+                </li>
+              </ul>
+            )}
           </div>
-          <a className="hidden lg:block">
-            <Image src={logo} className="object-contain w-auto" alt="logo" />
-          </a>
+          <div className="hidden lg:flex">
+            <Link href={"/"}>
+              <Image src={logo} className="object-contain w-auto" alt="logo" />
+            </Link>
+          </div>
         </div>
         <div className="navbar-center hidden lg:flex bg-white bg-opacity-60 border-2 border-white text-[#8987A1] rounded-lg">
           <ul className="menu menu-horizontal px-1 flex items-center">
