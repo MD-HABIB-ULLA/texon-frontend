948f05fde9e9e9b177b0742f02e366534d0194f8
peuldev <peul.islam71@gmail.com>
2024-09-10 15:59:58 +0600

    update

--- src/components/pages/Home/clients/clientsSlide.jsx
+++ src/components/pages/Home/clients/clientsSlide.jsx
@@ -18,15 +18,42 @@ export const ClientsSlide = () => {
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
           <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8">
-            <div className="relative  flex-shrink-0">
+            <div className="relative  flex-shrink-0 py-2 md:py-0">
               <Image
-                className=" shadow-md w-full"
+                className=" shadow-md w-full bg-cover rounded-xl md:rounded-none"
                 src={clients_image}
                 alt="Client Image"
               />
               <FaPlayCircle className="absolute inset-0 m-auto  text-6xl text-white opacity-80" />
             </div>
-            <div className="text-start space-y-4">
+            <div className="text-start space-y-4 px-2 md:px-2 py-5 md:py-0">
+              <h3 className="text-sm text-gray-500">May 8, 2020</h3>
+              <h1 className="text-2xl md:text-3xl font-bold">Mehedi Shamim</h1>
+              <h4 className="text-lg md:text-xl">
+                Head of Brand, Marketing, and Strategy at{" "}
+                <span className="font-medium">Apex Property</span>
+              </h4>
+              <p className="mt-2">
+                The Texon Limited team did an excellent job delivering a custom
+                software solution that fit our specific needs. Their technical
+                expertise and attention to detail made the project a success. We
+                were impressed with their ability to deliver the project on time
+                and within budget.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8">
+            <div className="relative  flex-shrink-0 py-2 md:py-0">
+              <Image
+                className=" shadow-md w-full bg-cover rounded-xl md:rounded-none"
+                src={clients_image}
+                alt="Client Image"
+              />
+              <FaPlayCircle className="absolute inset-0 m-auto  text-6xl text-white opacity-80" />
+            </div>
+            <div className="text-start space-y-4 px-2 md:px-2 py-5 md:py-0">
               <h3 className="text-sm text-gray-500">May 8, 2020</h3>
               <h1 className="text-2xl md:text-3xl font-bold">Mehedi Shamim</h1>
               <h4 className="text-lg md:text-xl">
