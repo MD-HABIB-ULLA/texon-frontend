4e8c44b16050c0ffd0ae37c45c133f15ec6c81c9
peuldev <peul.islam71@gmail.com>
2024-09-12 12:32:43 +0600

    blog image aded

--- src/app/(page)/blogs/page.jsx
+++ src/app/(page)/blogs/page.jsx
@@ -1,71 +1,88 @@
 import HeadingTitle from "@/components/common/title/headingTitle";
 import Image from "next/image";
+import blog_banner from "../../../../public/blogs/blog-banner.jpg";
 import chessmovecontent from "../../../../public/images/Chess move content.svg";
 import customvscms from "../../../../public/images/customvscms.svg";
 import edutech from "../../../../public/images/edutech.svg";
 
 const Page = () => {
   return (
-    <div className="max-w-screen-xl mx-auto px-2 md:pt-10">
-      <div className="py-10">
-        <HeadingTitle
-          heading={<>To helps the learn of the Tech world.</>}
-          subHeading={<>Read Blog</>}
-        />
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2">
-          <div>
-            <Image src={customvscms} alt="customvscms"></Image>
-            <div className="space-y-4">
-              <button className="py-1 px-4 rounded-full raleway bg-[#777777] text-white cursor-pointer font-medium mt-4">
-                Business
-              </button>
-              <p className="font-bold text-xl md:text-2xl lg:text-2xl">
-                Custom vs CMS Development Which is Better for Long Term Business
-              </p>
-              <p>
-                <span className="text-[#252432]">June 11, 2024</span>
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <Image src={edutech} alt="edutech"></Image>
-            <div className="space-y-4">
-              <button className="py-1 px-4 rounded-full raleway bg-[#777777] text-white cursor-pointer font-medium mt-4">
-                Technology
-              </button>
-              <p className="font-bold text-xl md:text-2xl lg:text-2xl">
-                Transforming Education with Cutting Edge EdTech Solutions
-              </p>
-              <p>
-                <span className="text-[#252432]">June 11, 2024</span>
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <Image src={chessmovecontent} alt="chessmovecontent"></Image>
-            <div className="space-y-4">
-              <button className="py-1 px-4 rounded-full raleway bg-[#777777] text-white cursor-pointer font-medium mt-4">
-                Technology
-              </button>
-              <p className="font-bold text-xl md:text-2xl lg:text-2xl">
-                Seeking a breakthrough in your current situation
-              </p>
-              <p>
-                <span className="text-[#252432]">June 11, 2024</span>
-              </p>
-            </div>
-          </div>
+    <>
+      <div className="relative mx-auto">
+        <div>
+          <Image
+            className="mx-auto w-full"
+            src={blog_banner}
+            alt="blog_banner"
+          />
         </div>
-        <div className="flex md:justify-center lg:justify-center mt-5 md:mt-0">
-          <button className="px-5 py-2 text-base rounded-lg raleway bg-primry hover:bg-blue text-white cursor-pointer font-medium">
-            View All vlogs
-          </button>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="text-white text-4xl font-bold">Blogs</h1>{" "}
         </div>
       </div>
-    </div>
+
+      <div className="max-w-screen-xl mx-auto px-2 md:pt-10">
+        <div className="py-10">
+          <HeadingTitle
+            heading={<>To helps the learn of the Tech world.</>}
+            subHeading={<>Read Blog</>}
+          />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2">
+            <div>
+              <Image src={customvscms} alt="customvscms"></Image>
+              <div className="space-y-4">
+                <button className="py-1 px-4 rounded-full raleway bg-[#777777] text-white cursor-pointer font-medium mt-4">
+                  Business
+                </button>
+                <p className="font-bold text-xl md:text-2xl lg:text-2xl">
+                  Custom vs CMS Development Which is Better for Long Term
+                  Business
+                </p>
+                <p>
+                  <span className="text-[#252432]">June 11, 2024</span>
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <Image src={edutech} alt="edutech"></Image>
+              <div className="space-y-4">
+                <button className="py-1 px-4 rounded-full raleway bg-[#777777] text-white cursor-pointer font-medium mt-4">
+                  Technology
+                </button>
+                <p className="font-bold text-xl md:text-2xl lg:text-2xl">
+                  Transforming Education with Cutting Edge EdTech Solutions
+                </p>
+                <p>
+                  <span className="text-[#252432]">June 11, 2024</span>
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <Image src={chessmovecontent} alt="chessmovecontent"></Image>
+              <div className="space-y-4">
+                <button className="py-1 px-4 rounded-full raleway bg-[#777777] text-white cursor-pointer font-medium mt-4">
+                  Technology
+                </button>
+                <p className="font-bold text-xl md:text-2xl lg:text-2xl">
+                  Seeking a breakthrough in your current situation
+                </p>
+                <p>
+                  <span className="text-[#252432]">June 11, 2024</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex md:justify-center lg:justify-center mt-5 md:mt-0">
+            <button className="px-5 py-2 text-base rounded-lg raleway bg-primry hover:bg-blue text-white cursor-pointer font-medium">
+              View All vlogs
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
