e94dae258f79d740d6665fc60f009f69e51685c0
peuldev <peul.islam71@gmail.com>
2024-09-13 12:03:56 +0600

    client title aded

--- src/components/pages/Home/clients/clients.jsx
+++ src/components/pages/Home/clients/clients.jsx
@@ -1,12 +1,24 @@
 "use client";
+import HeadingTitle from "@/components/common/title/headingTitle";
 import ClientsSlide from "./clientsSlide";
 
 export default function Clients() {
   return (
-    <div className="max-w-screen-xl mx-auto pt-0  pb-10 px-2">
-      <div>
-        <ClientsSlide />
+    <>
+      <div className="mt-5 md:mt-0">
+        <HeadingTitle
+          heading={
+            <>
+              What Our Clients Say <br /> About Us
+            </>
+          }
+        />
       </div>
-    </div>
+      <div className="max-w-screen-xl mx-auto pt-0  pb-10 px-2">
+        <div>
+          <ClientsSlide />
+        </div>
+      </div>
+    </>
   );
 }
