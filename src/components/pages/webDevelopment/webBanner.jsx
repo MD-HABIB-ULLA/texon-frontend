8be76eed654739a9e878981e7344c4daade90955
peuldev <peul.islam71@gmail.com>
2024-09-17 13:47:29 +0600

    web bannar aded

--- src/components/pages/webDevelopment/webBanner.jsx
+++ src/components/pages/webDevelopment/webBanner.jsx
@@ -1,7 +1,19 @@
+import { FaCalendarAlt } from "react-icons/fa";
+
 export default function WebBanner() {
   return (
-    <div className="max-w-screen-xl mx-auto px-2">
-      <h1>webBanner.jsx</h1>
+    <div className="bg-[#111827] py-10">
+      <div className="max-w-screen-xl mx-auto px-4">
+        <div className="flex flex-col items-center">
+          <h1 className="text-white text-center text-3xl md:text-4xl font-medium mb-4">
+            Web Design & Development
+          </h1>
+          <button className="flex items-center px-5 py-2 justify-center font-medium bg-primry hover:bg-blue text-white mt-4">
+            <FaCalendarAlt className="mr-2" />
+            Request a Demo
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
