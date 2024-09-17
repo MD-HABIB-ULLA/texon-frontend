833cfc92d8ea67b970539d81943d14992b08226c
peuldev <peul.islam71@gmail.com>
2024-09-17 12:49:36 +0600

    discuss from update

--- src/components/pages/Services/discussFrom/discussFrom.jsx
+++ src/components/pages/Services/discussFrom/discussFrom.jsx
@@ -1,11 +1,18 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import { FiCheck } from "react-icons/fi";
 import discussImage from "../../../../../public/images/product development.svg";
-
 export default function DiscussForm() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
   return (
     <div className="max-w-screen-xl mx-auto bg-primry my-20 rounded-md">
       <div className="px-2 md:px-8 flex flex-col md:flex-row">
-        {/* Text */}
+        {/* Text Section */}
         <div className="text-white p-2 md:p-5 md:w-1/2">
           <p className="text-lg mb-2 font-medium">Got a project in mind?</p>
           <h1 className="text-3xl md:text-4xl mb-4 font-semibold">
@@ -18,49 +25,61 @@ export default function DiscussForm() {
           />
         </div>
 
-        {/* Form */}
+        {/* Form Section */}
         <form className="card-body md:w-1/2 p-2 md:p-4 lg:mt-10">
-          <div className="form-control mb-4">
-            <label className="label">
-              <span className="label-text text-white">Full Name</span>
+          {/* Full Name Input */}
+          <div className="form-control mb-6">
+            <label htmlFor="name" className="block mb-2 text-white">
+              Full Name
             </label>
             <input
               type="text"
-              placeholder="Your Name Here"
-              className="input input-bordered w-full"
-              required
+              id="name"
+              name="name"
+              className="w-full px-3 py-2 border border-gray rounded focus:outline-none focus:ring-1 focus:border-yellow"
+              placeholder="Your name here"
             />
           </div>
-          <div className="flex flex-col md:flex-row gap-4 mb-4">
+
+          {/* Email and Phone Number Inputs */}
+          <div className="flex flex-col md:flex-row gap-4 mb-6">
             <div className="form-control flex-1">
-              <label className="label">
-                <span className="label-text text-white">Company Name</span>
+              <label htmlFor="phone" className="block mb-2 text-white">
+                Company Name
               </label>
               <input
                 type="text"
-                placeholder="Ex. Texon Ltd"
-                className="input input-bordered w-full"
-                required
+                id="company"
+                name="company"
+                className="w-full px-3 py-2 border border-gray rounded focus:outline-none focus:ring-1 focus:border-yellow"
+                placeholder="Enter your phone number"
               />
             </div>
             <div className="form-control flex-1">
-              <label className="label">
-                <span className="label-text text-white">Email</span>
+              <label htmlFor="email" className="block mb-2 text-white">
+                Email*
               </label>
               <input
                 type="email"
-                placeholder="you@example.com"
-                className="input input-bordered w-full"
-                required
+                id="email"
+                name="email"
+                className="w-full px-3 py-2 border border-gray rounded focus:outline-none focus:ring-1 focus:border-yellow"
+                placeholder="Enter your email"
               />
             </div>
           </div>
-          <div className="flex flex-col md:flex-row gap-4 mb-4">
+
+          {/* Service Required and Project Budget Dropdowns */}
+          <div className="flex flex-col md:flex-row gap-4 mb-6">
             <div className="form-control flex-1">
-              <label className="label">
-                <span className="label-text text-white">Service Required*</span>
+              <label htmlFor="service" className="block mb-2 text-white">
+                Service Required*
               </label>
-              <select className="select select-bordered w-full">
+              <select
+                id="service"
+                name="service"
+                className="w-full px-3 py-2 border border-gray rounded focus:outline-none focus:ring-1 focus:border-yellow bg-white text-black"
+              >
                 <option disabled value="">
                   Select Your Service
                 </option>
@@ -71,10 +90,14 @@ export default function DiscussForm() {
               </select>
             </div>
             <div className="form-control flex-1">
-              <label className="label">
-                <span className="label-text text-white">Project Budget*</span>
+              <label htmlFor="budget" className="block mb-2 text-white">
+                Project Budget*
               </label>
-              <select className="select select-bordered w-full">
+              <select
+                id="budget"
+                name="budget"
+                className="w-full px-3 py-2 border border-gray rounded focus:outline-none focus:ring-1 focus:border-yellow bg-white text-black"
+              >
                 <option disabled value="">
                   Select Your Range
                 </option>
@@ -85,28 +108,38 @@ export default function DiscussForm() {
               </select>
             </div>
           </div>
-          <div className="form-control mb-4">
-            <label className="label">
-              <span className="label-text text-white">Project Details*</span>
+
+          {/* Message Input */}
+          <div className="form-control mb-6">
+            <label htmlFor="message" className="block mb-2 text-white">
+              Project details*
             </label>
             <textarea
-              className="textarea textarea-bordered w-full"
+              id="message"
+              name="message"
+              className="w-full px-3 py-2 border border-gray rounded focus:outline-none focus:ring-1 focus:border-yellow"
               placeholder="Tell us more about your idea"
-              required
-            ></textarea>
-          </div>
-          <div className="flex items-center mb-4">
-            <input
-              type="checkbox"
-              defaultChecked
-              className="checkbox mr-2 rounded-none text-white bg-white"
             />
-            <p className="text-white">
-              I agree to the Terms of Service and Privacy Policy.
-            </p>
           </div>
+
+          {/* Terms Checkbox */}
+          <div className="flex items-center">
+            <div
+              className={`w-5 h-5 mr-2 border-2 border-white bg-transparent flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out ${
+                isChecked ? "border-white" : ""
+              }`}
+              onClick={handleCheckboxChange}
+            >
+              {isChecked && <FiCheck className="text-white text-lg" />}
+            </div>
+            <label htmlFor="terms" className="text-white text-base">
+              I agree to the terms of Service and Privacy Policy.
+            </label>
+          </div>
+
+          {/* Submit Button */}
           <div className="form-control lg:mb-0 md:mb-5 mb-5">
-            <button className="btn bg-black text-white w-full md:w-auto border-none hover:bg-yellow">
+            <button className="btn bg-black text-white text-lg font-normal w-full md:w-auto border-none hover:bg-yellow">
               Send Inquiry
             </button>
           </div>
